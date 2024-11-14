1343867ea9ad6a78d19fdaf0f5acbd6941fce1ec
Cindy <cindy870709@gmail.com>
2024-11-14 17:16:30 +0800

    Weather API application

---
+++ README.md
@@ -0,0 +1,77 @@
+# Weather API
+
+Sample solution for the [weather API](https://roadmap.sh/projects/weather-api-wrapper-service) challenge from [roadmap.sh](https://roadmap.sh/roadmaps).
+
+## Intoduction
+
+This project is a Weather API that fetches weather data from third-party sources. Its uses Flask Limiter for rate limiting and Redis caching for imporved performance. Sensitive information is stored using environment variables, and errors are logged with Python's logging module.
+
+## Feature
+
+- **Weather Data Retrival**: Fetches real-time weather information from third-party service(Visual Crossing Weather API).
+- **Caching**: Implements Redis caching to improve performance by reducing redundant data requests.
+- **Rate Limiting**: Uses Flask Limiter to control the frequency of API requests and avoid exceeding limits.
+- **Enviornment Variable Management**: Sensitive data(like API keys) is securely stored using enviornment variables.
+- **Error Logging**: Logs errors and important events using Pythin's built-in logging module for better debugging and monitoring.
+
+## Tools & Libraries
+
+- Flask: A lightweight web framework for building the API.
+- Flask-Limiter: Used for rate-limiting API request.
+- Redis: A database used for caching to imporve performance.
+- urllib3: A HTTP library for making requests to external APIs.
+- certifi: Provides SSL certificates to ensure secure HTTP requests.
+- Python-dotenv: Loads environment variables from a `.env` file to securely store sensituve information like API keys.
+- Logging: Python's built-in logging module for tracking errors and application events.
+
+## Browser Interface(HTML)
+
+This Weather API includes a user-friendly HTML interface that allows you to easily query weather data directly through your web browser.
+
+1. Accessing the interface:
+   Once the Weather API server is running, you can open your browser and nevigate to :
+
+```shell
+http://127.0.0.1:5500
+```
+
+2. Using the interface:
+
+- **Location(Required)**: Enter the name of the Location(e.g.,"UK") in the appropriate input filed.
+- **City(Optional)**: Enter the name of the city (e.g., "London").
+- **Start date(Optional)**: Select the start date for the weather data.
+- **End date(Optional)**: Select the end date for the weather data.
+- **Daily Data (Optional)**:
+  - If unchecked, the result will include hourly data for each day.
+  - If checked, the result will only show daily summaries without hourly breakdowns.
+- **Search**: After filling in the location and optionally selecting the date, click the search button to retrieve the weather data for the specified location and date range.
+
+![Weather App Demo](image/weather_data.gif)
+
+## Installation
+
+To get started with this project, clone the repository to your local machine:
+
+```shell
+# Clone the repository
+git clone https://github.com/HsinLing-Chang/Weather-API.git
+
+# Navigate into the cloned project directory
+cd Weather-API
+```
+
+## How to run
+
+Before accessing the browser interface, you need to start the application by running app.py from the command line.
+
+1. Start the Flask Server
+   Open the terminal and nevigate to the project directory where app.py is located.
+
+```shell
+python app.py
+```
+
+This will start the server on 127.0.0.1 (localhost) at port 5500.
+
+2. Open the Browser Interface
+   After starting the server, open your web browser and go to `http://127.0.0.1:5000` to access the HTML interface.
---
+++ app.py
@@ -0,0 +1,126 @@
+import os
+from dotenv import load_dotenv
+import json
+import logging
+from datetime import datetime
+import redis
+import urllib3
+import certifi
+from flask import Flask, request, render_template
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
+load_dotenv()
+# initalize Flask app
+app = Flask(__name__)
+# logging message setting
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s- %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+
+# env setting
+redis_host = os.getenv("HOST")
+redis_port = os.getenv("PORT")
+redis_password = os.getenv("PASSWORD")
+API_KEY = os.getenv("API_KEY")
+
+r = redis.Redis(
+    host=redis_host,
+    port=redis_port,
+    password=redis_password,
+    decode_responses=True)
+
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["200 per day", "5 per minute"],
+    storage_uri=f"redis://:{redis_password}@{redis_host}:{redis_port}/0"
+)
+
+
+@app.get("/")
+def home():
+    return render_template("index.html")
+
+
+@app.get("/weather")
+def weather():
+    location = request.args.get("location")
+    city = request.args.get("city", "")
+    date_start = request.args.get("start", "")
+    date_end = request.args.get("end", "")
+    daily_data = request.args.get("daily-data", "")
+
+    try:
+        # check if date range exceeds 15 days
+        if date_start and date_end:
+            today = datetime.now().date()
+            end = datetime.strptime(date_end, "%Y-%m-%d").date()
+            date_diff = end - today
+            if date_diff.days > 14:
+                raise ValueError(
+                    "Date range exceeds the limit. Only forecasts for the next 15 days are available.")
+        else:
+            date_start = date_end = ""
+    except ValueError as e:
+        return render_template("Error.html", error=e)
+
+    if daily_data:
+        include = f"&include={daily_data}"
+    else:
+        include = daily_data
+    if city:
+        city = f"%2C{city}"
+
+    redis_key = location + city + date_start + date_end + daily_data
+
+    weather_data = r.get(redis_key)
+    if weather_data:
+        logging.info("Cache hit: Data retrieved from Redis")
+        return json.loads(weather_data)
+
+    else:
+        try:
+            # setting PoolManager instance
+            http = urllib3.PoolManager(
+                cert_reqs="CERT_REQUIRED",
+                ca_certs=certifi.where()
+            )
+            WEATHER_URL = (f'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/'
+                           f'{location}{city}/{date_start}/{date_end}?unitGroup=metric{include}&key={API_KEY}&contentType=json')
+
+            weather_data = http.request("GET", WEATHER_URL, timeout=5)
+
+            if weather_data.status == 200:
+                weather_data_decode = weather_data.data.decode('utf-8')
+                r.set(redis_key, weather_data_decode, ex=3600)
+                logging.info("Cache miss: Fetching data from API")
+                return json.loads(weather_data_decode)
+            elif weather_data.status == 400:
+                logging.debug(f"API request failed with status {
+                    weather_data.status}")
+                error = "Weather API Error: Invalid location parameter value.."
+                return render_template("Error.html", error=error)
+            else:
+                logging.debug(f"API request failed with status {
+                    weather_data.status}")
+                error = "Weather API Error "
+                return render_template("Error.html", error=error)
+        except urllib3.exceptions.HTTPError as e:
+            logging.error(f"HTTP error occured: {e}")
+            error = f"HTTP error occured: {e}"
+            return render_template("Error.html", error=error)
+        except urllib3.exceptions.TimeoutError as e:
+            logging.error(f"Request timed out: {e}")
+            error = f"Request timed out while trying to connect: {e}"
+            return render_template("Error.html", error=error)
+        except Exception as e:
+            logging.error(f"An unexpected error occurred: {e}")
+            error = "An unexpected error occurred while fetching weather data."
+            return render_template("Error.html", error=error)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5500)
